e620ee2c59f8b1dd6804418352aa1ea14a68068e
Pvitaly91 <Pvitaly91@gmail.com>
2025-07-26 14:59:49 +0300

    feat: add tailwind via CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
     <title>Laravel App</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    @yield('content')
+<body class="bg-gray-100 font-sans antialiased">
+    <div class="container mx-auto p-4">
+        @yield('content')
+    </div>
 </body>
 </html>
